00f1a75a4bc84652c2cae9a6baf8688df5ee574b
Maximilian Techritz <maximilian.techritz@sap.com>
2026-03-06 20:56:57 +0100

    fix(e2e): image version service provider

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -34,7 +34,7 @@ func TestMain(m *testing.M) {
 		ServiceProviders: []providers.ServiceProviderSetup{
 			{
 				Name:  "flux",
-				Image: "ghcr.io/openmcp-project/images/service-provider-flux:0.0.1",
+				Image: "ghcr.io/openmcp-project/images/service-provider-flux:0.0.1-linux-arm64",
 			},
 		},
 	}
