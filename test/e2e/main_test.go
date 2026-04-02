c740179f70e7b88d008095209d53da89eb270f2e
Maximilian Techritz <maximilian.techritz@sap.com>
2026-04-02 09:04:31 +0200

    fix: intial version

--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -36,7 +36,7 @@ func TestMain(m *testing.M) {
 		ServiceProviders: []providers.ServiceProviderSetup{
 			{
 				Name:               "flux",
-				Image:              "ghcr.io/openmcp-project/images/service-provider-flux:0.0.1",
+				Image:              "ghcr.io/openmcp-project/images/service-provider-flux:v0.1.0",
 				LoadImageToCluster: true,
 			},
 		},
